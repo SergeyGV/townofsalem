faf3ba05cd76a00ded4f0b902f0bfa1aa4fd0d2a
SergeyGV <sergey.gayvoronskiy@mail.utoronto.ca>
2016-05-17 18:47:08 -0400

    Completed the RoleStack GUI Class

--- Generator/src/gui/Display.java
+++ Generator/src/gui/Display.java
@@ -1,6 +1,8 @@
 package gui;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -17,8 +19,12 @@ public class Display {
 
     public Display() {
 
+        Panel thing = new Panel();
+        thing.setLayout(null);
+        thing.setBounds(0,0,1000,1000);
         mainFrame = new Frame("Town Of Salem Simulator");
         mainFrame.setSize(FrameWidth, FrameLength);
+        mainFrame.setResizable(false);
         mainFrame.setLayout(null); // Locations will be done manually
         mainFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
@@ -27,8 +33,30 @@ public class Display {
         });
         roleList = new RoleStack();
         mainFrame.add(roleList.returnPanel());
+        Button butt = new Button("Add role");
+        Button butter = new Button("Remove role");
+        butt.setBounds(400, 400, 130, 50);
+        butter.setBounds(400, 450, 130, 50);
+        butt.setActionCommand("Add");
+        butter.setActionCommand("Remove");
+        butt.addActionListener(new ButtonListener());
+        butter.addActionListener(new ButtonListener());
+        thing.add(butt);
+        thing.add(butter);
+        mainFrame.add(thing);
         mainFrame.setVisible(true); // Should be done as the very last step
 
+    }
+
+    public class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+            if (command.equals("Add")) {
+                roleList.pushRole("Town Investigative");
+            } else {
+                roleList.popRole();
+            }
+        }
     }
 
 }
--- Generator/src/gui/RoleStack.java
+++ Generator/src/gui/RoleStack.java
@@ -2,7 +2,6 @@ package gui;
 
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Stack;
 
 /**
  * RoleStack - In charge of controlling the display for the chosen
@@ -10,36 +9,83 @@ import java.util.Stack;
  */
 public class RoleStack {
 
-    private Stack dataStack = new Stack();
+    private ArrayList<Label> roleList;
     private Panel rolePanel;
     private Font mainFont;
     private String chosenFont = "Calibra";
+    private int currentLabel; // Tracker for which element in the stack we are on
     private int fontSize = 20;
-    private int initx = 30;
-    private int inity = 30;
-    private int boxlength = 200;
-    private int boxwidth = 30;
+    private int initx = 60; // X coordinate of the stack of boxes
+    private int inity = 60; // Y coordinate of the corner of the top left box
+    private int boxlength = 200; // Length of each box
+    private int boxwidth = 30; // Height of each box
 
     public RoleStack() {
 
+        currentLabel = 0;
         rolePanel = new Panel();
         rolePanel.setLayout(null);
-        rolePanel.setBounds(initx, inity, 1000, 1000);
+        rolePanel.setBounds(initx, inity, boxlength, boxwidth*15);
         mainFont = new Font(chosenFont, Font.PLAIN, fontSize);
-        Label chosenRole;
+        roleList = new ArrayList<>();
+        Label role;
         for (int i = 0; i < 15; i++) {
-            chosenRole = new Label();
-            chosenRole.setAlignment(Label.LEFT);
-            chosenRole.setText("Town Investigative");
-            chosenRole.setFont(mainFont);
-            chosenRole.setBounds(initx, inity + boxwidth*i, boxlength, boxwidth);
-            rolePanel.add(chosenRole);
+            role = new Label("");
+            role.setAlignment(Label.LEFT);
+            role.setFont(mainFont);
+            role.setBounds(0, boxwidth*i, boxlength, boxwidth);
+            roleList.add(role);
+            rolePanel.add(role);
         }
 
     }
 
     /**
+     * "Pushes" a new role to the top of the displayed stack, as long as the
+     * size limit hasn't been exceeded
+     *
+     * @param role The role that is desired to be added
+     */
+    public void pushRole(String role) {
+
+        if (currentLabel < 15) {
+            roleList.get(currentLabel).setText(role);
+            currentLabel++;
+        }
+
+    }
+
+    /**
+     * Removes the top role on the current panel
+     */
+    public void popRole() {
+
+        if (currentLabel != 0) {
+            currentLabel--;
+            roleList.get(currentLabel).setText("");
+        }
+
+    }
+
+    /**
+     * Returns the entire role list that is in the displayed stack, formatted
+     * for processing and generating the role list.
+     *
+     * @return The role list
+     */
+    public ArrayList<String> returnRoleList() {
+
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < currentLabel; i++) {
+            result.add(roleList.get(i).getText());
+        }
+        return (result);
+
+    }
+
+    /**
      * Returns the Panel used by this instance of the RoleStack class
+     *
      * @return The panel worked upon by this class
      */
     public Panel returnPanel() {
