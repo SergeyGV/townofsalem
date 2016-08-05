5f9b57d43b5259c622f5f21eb15ddacbec365fd4
SergeyGV <sergey.gayvoronskiy@mail.utoronto.ca>
2016-08-05 18:06:23 -0400

    Removed old methods in comments, redirected main

--- Generator/src/main/Controller.java
+++ Generator/src/main/Controller.java
@@ -8,78 +8,10 @@ import actions.Actions;
 import actions.RoleInfo;
 import generation.Generator;
 import generation.RoleStorage;
+import validator.MainValidator;
 
 public class Controller {
-	
-	/*
 
-	private String getTownRole() {
-		int totalSize = TK.size() + TI.size() + TS.size();
-		int chosen = randomizer.nextInt(totalSize + TP.size());
-		if (totalSize < chosen) {
-			return(getRole(TP));
-		}
-		totalSize -= TS.size();
-		if (totalSize < chosen) {
-			return(getRole(TS));
-		}
-		totalSize -= TI.size();
-		if (totalSize < chosen) {
-			return(getRole(TI));
-		}
-		return(getRole(TK));
-	}
-
-	private String getAnyRole() {
-		int totalSize = TK.size() + TI.size() + TS.size() + TP.size()
-		+ Mafia.size() + NK.size() + NE.size() + NB.size();
-		int chosen = randomizer.nextInt(totalSize + 1);
-		if (chosen == totalSize) { // Vampire special case
-			TK.add(TK.size(), "Vampire Hunter");
-			return("Vampire");
-		}
-		totalSize -= NB.size();
-		if (totalSize < chosen) {
-			return(getRole(NB));
-		}
-		totalSize -= NE.size();
-		if (totalSize < chosen) {
-			return(getRole(NE));
-		}
-		totalSize -= NK.size();
-		if (totalSize < chosen) {
-			return(getRole(NK));
-		}
-		totalSize -= Mafia.size();
-		if (totalSize < chosen) {
-			return(getRole(Mafia));
-		}
-		totalSize -= TP.size();
-		if (totalSize < chosen) {
-			return(getRole(TP));
-		}		
-		totalSize -= TS.size();
-		if (totalSize < chosen) {
-			return(getRole(TS));
-		}		
-		totalSize -= TK.size();
-		if (totalSize < chosen) {
-			return(getRole(TK));
-		}		
-		return(getRole(TI));
-	}
-
-	private String getRole(ArrayList<String> RoleList) {
-		int chosenRole = randomizer.nextInt(RoleList.size());
-		String roleName = RoleList.get(chosenRole);
-		if (Unique.contains(roleName)) {
-			Unique.remove(roleName);
-			RoleList.remove(roleName);
-		}
-		return (roleName);
-	}
-
-	*/
 
 	public static void main(String[] args) {
 
@@ -101,10 +33,16 @@ public class Controller {
 		roles.add("Any");
 		roles.add("Any");
 		roles.add("Any");
-		Generator gen = new Generator(new RoleStorage(), roles);
+		RoleStorage storage = new RoleStorage();
+		Generator gen = new Generator(storage, roles);
+		MainValidator val = new MainValidator(storage, roles);
+		for (String inf: val.validate()) {
+			System.out.println(inf);
+		}
+		/*
 		for (String role: gen.Generate()) {
 			System.out.println(role);
-		}
+		} */
 		//new RoleInfo(); // Generate investigative results
 		//Actions test = new Actions();
 		//test.generate(gen.Generate());
