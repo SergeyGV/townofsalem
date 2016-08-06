59c698682b67ad4ced6412590cbd284c5dd7842e
SergeyGV <sergey.gayvoronskiy@mail.utoronto.ca>
2016-08-06 19:10:30 -0400

    Added MafiaValidator - Checks to see if there is a valid Mafia

---
+++ Generator/src/validator/MafiaValidator.java
@@ -0,0 +1,19 @@
+package validator;
+
+
+import java.util.ArrayList;
+
+public class MafiaValidator {
+
+    public static ArrayList<String> validate(MainValidator val) {
+
+        ArrayList<String> infractions = new ArrayList<>();
+        for (String role: val.roles) {
+
+        }
+        return infractions;
+
+    }
+
+
+}
