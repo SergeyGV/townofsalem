bfb3511f1a9cd454eee698e8e78585558c0b4d05
SergeyGV <sergey.gayvoronskiy@mail.utoronto.ca>
2016-08-19 13:28:56 -0400

    Updated Consort to temp-working conditions(need validMafTarget)

--- Generator/src/roles/Consort.java
+++ Generator/src/roles/Consort.java
@@ -1,35 +1,37 @@
 package roles;
 
+import actions.RoleInfo;
+
 public class Consort extends RoleControl {
 
     public Consort(String name, int num) {
         super(name, num);
     }
 
-    public void Process() { /*
+    public void Process() {
 
-        player = Players.get(Consort);
-        if (!player.jailed) {
-            target = validMafTarget();
-            player.NightAction = "You have decided to roleblock " + String.valueOf(target) + "(" +
-                    Players.get(target).name + ") tonight.";
+        if (!jailed) {
+            target = validTownTarget(playerNum);
+            // !!!TEMPORARY!!! Implement validMafTarget, delete above line, uncomment below line
+            //target = validMafTarget();
+            nightAction = "You have decided to roleblock " + String.valueOf(target) + "(" +
+                    players.get(target).roleName + ") tonight.";
             target = checkTargetSwitch(target); // Process both witching and transporting
-            MafiaVisits.add(target);
-            if (Players.get(target).name.equals("Serial Killer")) {
+            /*if (players.get(target).name.equals("Serial Killer")) {
                 player.attackers.add("SKVisit");
-                Players.get(target).activity.add("SKBlock");
-                Players.get(target).blocked = true;
-            } else if (Players.get(target).jailed) {
-                Players.get(target).activity.add("RBJail");
-            } else if (RoleInfo.RoleBlockImmune.contains(Players.get(target).name)) {
-                Players.get(target).activity.add("RBImmune");
+                players.get(target).activity.add("SKBlock");
+                players.get(target).blocked = true;
+            } else <-- Cut off from below statement */
+            if (players.get(target).jailed) {
+                players.get(target).activity.add("RBJail");
+            } else if (RoleInfo.RoleBlockImmune.contains(players.get(target).roleName)) {
+                players.get(target).activity.add("RBImmune");
                 checkVetVisit(target);
             } else {
-                Players.get(target).activity.add("RB");
-                Players.get(target).blocked = true;
+                players.get(target).activity.add("RB");
+                players.get(target).blocked = true;
             }
-            AllVisits.get(target).add(Consort);
-        } */
+        }
 
     }
 
