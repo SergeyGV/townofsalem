6ff3f43f4b95e8aacac23b1b1cf3501e5e87d4a3
SergeyGV <sergey.gayvoronskiy@mail.utoronto.ca>
2016-09-26 17:07:51 -0400

    Begun Layout organization

--- Generator/src/gooey/Drawer.java
+++ Generator/src/gooey/Drawer.java
@@ -6,18 +6,22 @@ import java.awt.*;
 public class Drawer {
 
     JFrame mainFrame;
-    JPanel controlPanel;
+    JPanel stackPanel;
+    JPanel printerPanel;
+    JButton removeButton;
+    JButton generateButton;
 
     public Drawer() {
 
         mainFrame = new JFrame();
-        controlPanel = new JPanel();
+        stackPanel = new JPanel();
+        printerPanel = new JPanel();
+        removeButton = new JButton("REMOVE");
+        generateButton = new JButton("GENERATE");
 
     }
 
     public void drawGUI() {
-
-        GridBagConstraints constraints;
 
         // Set up the Frame
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -26,24 +30,47 @@ public class Drawer {
         Container pane = mainFrame.getContentPane();
         pane.setLayout(new GridBagLayout());
 
-        // Set up the controlling JPanel
-        constraints = new GridBagConstraints();
+        // Set up the control JPanel
+        stackPanel.setLayout(new GridLayout(15, 1));
+        stackPanel.setBackground(Color.CYAN);
+        pane.add(stackPanel, setUpStackPanel());
+
+        // Set up the printer JPanel
+        printerPanel.setLayout(new GridLayout(1, 1));
+        printerPanel.setBackground(Color.lightGray);
+        pane.add(printerPanel, setUpPrinterPanel());
+
+        // Show it all
+        mainFrame.pack();
+        mainFrame.setVisible(true);
+
+    }
+
+    private GridBagConstraints setUpStackPanel() {
+
+        GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        constraints.ipadx = 300;
+        constraints.ipady = 300;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        return (constraints);
+
+    }
+
+    private GridBagConstraints setUpPrinterPanel() {
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 1;
         constraints.gridy = 0;
         constraints.gridwidth = 5;
         constraints.gridheight = 5;
         constraints.ipadx = 500;
         constraints.ipady = 500;
-        //constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.weightx = 0.5;
-        constraints.weighty = 0.5;
-        controlPanel.setLayout(new GridBagLayout());
-        controlPanel.setBackground(Color.lightGray);
-        pane.add(controlPanel, constraints);
-
-        // Show it all
-        mainFrame.pack();
-        mainFrame.setVisible(true);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        return (constraints);
 
     }
 
