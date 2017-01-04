afe7305f1d1e98e47983e768ce0220c4d286c4b5
SergeyGV <sergey.gayvoronskiy@mail.utoronto.ca>
2017-01-04 10:53:17 -0500

    New(ish) GUI work to be refactored

--- Generator/src/gooey/ContainerSetter.java
+++ Generator/src/gooey/ContainerSetter.java
@@ -4,10 +4,87 @@ import java.awt.*;
 
 public class ContainerSetter {
 
-    public static GridBagConstraints setUpGenerateButton() {
+    public static GridBagConstraints setUpLeftPanel() {
 
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        //constraints.ipadx = 200;
+        //constraints.ipady = 550;
+        constraints.weightx = 0;
+        constraints.weighty = 0;
+        constraints.insets = new Insets(20, 20, 20, 20);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        return (constraints);
+
+    }
+
+    public static GridBagConstraints setUpCenterPanel() {
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        constraints.ipadx = 200;
+        constraints.ipady = 550;
+        constraints.insets = new Insets(20, 20, 20, 20);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        return (constraints);
+
+    }
+
+    public static GridBagConstraints setUpRightPanel() {
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 2;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        constraints.ipadx = 500;
+        constraints.ipady = 550;
+        constraints.insets = new Insets(20, 20, 20, 20);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        return (constraints);
+
+    }
+
+    public static GridBagConstraints setUpCategoryPanel() {
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        constraints.ipadx = 200;
+        constraints.ipady = 140;
+        constraints.insets = new Insets(0, 0, 10, 0);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        return (constraints);
+
+    }
+
+    public static GridBagConstraints setUpRolePanel() {
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        constraints.ipadx = 200;
+        constraints.ipady = 400;
+        constraints.insets = new Insets(0, 0, 0, 0);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        return (constraints);
+
+    }
+
+    public static GridBagConstraints setUpGenerateButton() {
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 1;
         constraints.gridy = 4;
         constraints.gridwidth = 1;
         constraints.gridheight = 1;
@@ -22,7 +99,7 @@ public class ContainerSetter {
     public static GridBagConstraints setUpRemoveButton() {
 
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 0;
+        constraints.gridx = 1;
         constraints.gridy = 3;
         constraints.gridwidth = 1;
         constraints.gridheight = 1;
@@ -37,7 +114,7 @@ public class ContainerSetter {
     public static GridBagConstraints setUpStackPanel() {
 
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 0;
+        constraints.gridx = 1;
         constraints.gridy = 0;
         constraints.gridwidth = 1;
         constraints.gridheight = 3;
@@ -52,7 +129,7 @@ public class ContainerSetter {
     public static GridBagConstraints setUpPrinterPanel() {
 
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.gridx = 1;
+        constraints.gridx = 2;
         constraints.gridy = 0;
         constraints.gridwidth = 5;
         constraints.gridheight = 5;
--- Generator/src/gooey/Drawer.java
+++ Generator/src/gooey/Drawer.java
@@ -6,16 +6,26 @@ import java.awt.*;
 public class Drawer {
 
     JFrame mainFrame;
+    JPanel leftPanel;
+    JPanel centerPanel;
+    JPanel rightPanel;
     JPanel stackPanel;
     JPanel printerPanel;
+    JPanel categoryPanel;
+    JPanel rolePanel;
     JButton removeButton;
     JButton generateButton;
 
     public Drawer() {
 
         mainFrame = new JFrame();
+        leftPanel = new JPanel();
+        centerPanel = new JPanel();
+        rightPanel = new JPanel();
         stackPanel = new JPanel();
         printerPanel = new JPanel();
+        categoryPanel = new JPanel();
+        rolePanel = new JPanel();
         removeButton = new JButton("REMOVE");
         generateButton = new JButton("GENERATE");
 
@@ -31,6 +41,44 @@ public class Drawer {
         pane.setLayout(new GridBagLayout());
         pane.setBackground(Color.GRAY);
 
+        // Set up the left JPanel
+        leftPanel.setLayout(new GridBagLayout());
+        leftPanel.setBackground(Color.DARK_GRAY);
+        pane.add(leftPanel, ContainerSetter.setUpLeftPanel());
+
+
+        // Set up the category JPanel
+        categoryPanel.setLayout(new GridLayout(4, 1));
+        categoryPanel.setBackground(Color.green);
+        leftPanel.add(categoryPanel, ContainerSetter.setUpCategoryPanel());
+
+        for (int i = 0; i < 4; i++) {
+            JButton butt = new JButton("test");
+            categoryPanel.add(butt);
+        }
+
+        // Set up the role JPanel
+        rolePanel.setLayout(new GridLayout(14, 1));
+        rolePanel.setBackground(SystemColor.green);
+        leftPanel.add(rolePanel, ContainerSetter.setUpRolePanel());
+
+        for (int i = 0; i < 14; i++) {
+            JButton butt = new JButton("test");
+            rolePanel.add(butt);
+        }
+
+        // Set up the center JPanel
+        rightPanel.setLayout(new GridBagLayout());
+        rightPanel.setBackground(Color.DARK_GRAY);
+        pane.add(rightPanel, ContainerSetter.setUpRightPanel());
+
+        // Set up the right JPanel
+        centerPanel.setLayout(new GridLayout(1, 1));
+        centerPanel.setBackground(Color.DARK_GRAY);
+        pane.add(centerPanel, ContainerSetter.setUpCenterPanel());
+
+        /*
+
         // Set up the stack JPanel
         stackPanel.setLayout(new GridLayout(15, 1));
         stackPanel.setBackground(Color.CYAN);
@@ -43,11 +91,7 @@ public class Drawer {
         // Set up the generate JButton
         generateButton.setBackground(Color.RED);
         pane.add(generateButton, ContainerSetter.setUpGenerateButton());
-
-        // Set up the printer JPanel
-        printerPanel.setLayout(new GridLayout(1, 1));
-        printerPanel.setBackground(Color.lightGray);
-        pane.add(printerPanel, ContainerSetter.setUpPrinterPanel());
+        */
 
         // Show it all
         mainFrame.pack();
