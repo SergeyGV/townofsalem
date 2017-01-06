645bac624624b8995ccd030129ef2d5242d81ab8
SergeyGV <sergey.gayvoronskiy@mail.utoronto.ca>
2017-01-06 11:09:55 -0500

    Updated Disguisor to the newest version

--- Generator/src/roles/Disguisor.java
+++ Generator/src/roles/Disguisor.java
@@ -9,46 +9,23 @@ public class Disguisor extends RoleControl {
     public void Process() {
 
         if (!jailed) {
-            /*
-             * I try to simulate the best course of action when it comes to roles
-             * Nothing advanced, but nothing that is questionable
-             * That said, the existence of the Disguisor is the definition of questionable
-             * For the sake of the program, he will visit if a Forger or a Janitor is visiting
-             * And that is the ONLY time a Disguisor can ever accomplish his job properly
-             * Outside of the town being blind as a bat that is
-             * And even then, he would get severe flak from his team for this action
-             */
-            if (visitingMD == 0 || visitingDisg != playerNum) {
-                nightAction = "You have decided to do nothing this night.";
-                target = 0;
-            } else {
-                nightAction = "You have decided to disguise as " + String.valueOf(mafTarget) + "(" +
-                        players.get(mafTarget).roleName + ") tonight.";
-                target = mafTarget;
-            }
+            // Always visits, add user set odds of visiting later
+            // Do disguisors visit marked targets? Assume not for now, but ask later
+            target = validNonDeathMafTarget();
+            nightAction = "You have decided to disguise as " + String.valueOf(target) + "(" +
+                    players.get(target).roleName + ") tonight.";
             target = checkTargetSwitch(target);
-            // Only have target == 0 be a do nothing case, the witched if is only for the message
-            if (target == 0) {
-                if (witched == 0) {
-                    nightResult = "You did not perform your night ability.";
-                }
-            } else if (!blocked) {
-                // Oh you poor sod... hope the town has the attention span of a rock
+            // Allow him to disguise as himself due to Witch(does nothing anyway)
+            if (!blocked) {
                 mafVisits.add(target);
                 players.get(target).visits.add(playerNum);
-                // If he doesn't die to the Vet or by someone else, start disguising
-                if (!checkVetVisit(target) && attackers.size() == 0) {
-                    if (players.get(target).disguised) {
-                        activity.add("DisgTaken");
-                    } else {
-                        players.get(target).disguised = true;
-                        // If target died, notify of successful disguise
-                        if (players.get(target).attackers.size() != 0) {
-                            activity.add("Disguised");
-                        }
-                    }
-                }
+                checkVetVisit(target);
+                // Allow a disguise to occur even if killed that night
+                // Also that's really it. For the purpose of this sim it's a plain visit
+                nightResult = "You have successfully disguised yourself(as a " +
+                        players.get(target).roleName + ")!";
             }
+
         }
 
     }
